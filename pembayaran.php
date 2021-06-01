30fc42cb146118bc2f5bb7d002b27cdb3806429d
yudhakrisnawan <yudhakrisnawan1922@gmail.com>
2021-06-01 12:09:23 +0700

    upload

---
+++ pembayaran.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+
+<?php
+include "connection/koneksi.php";
+session_start();
+ob_start();
+
+$id = $_SESSION['id_admin'];
+
+if(isset ($_SESSION['username'])){
+    $query = "select * from admin natural join level_user where id_admin = $id";
+
+    mysqli_query($conn, $query);
+    $sql = mysqli_query($conn, $query);
+    
+    while($r = mysqli_fetch_array($sql)){
+        $nama = $r['nama'];
+?>
+<html lang="en">
+<head>
+<title>Pembayaran</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="template/dashboard/css/bootstrap.min.css" />
+<link rel="stylesheet" href="template/dashboard/css/bootstrap-responsive.min.css" />
+<link rel="stylesheet" href="template/dashboard/css/fullcalendar.css" />
+<link rel="stylesheet" href="template/dashboard/css/matrix-style.css" />
+<link rel="stylesheet" href="template/dashboard/css/matrix-media.css" />
+<link href="template/dashboard/font-awesome/css/font-awesome.css" rel="stylesheet" />
+<link rel="stylesheet" href="template/dashboard/css/jquery.gritter.css" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<!--Header-part-->
+<div id="header">
+  <h1><a href="pembayaran.php">Pembayaran</a></h1>
+</div>
+<!--close-Header-part--> 
+
+
+<!--top-Header-menu-->
+<div id="user-nav" class="navbar navbar-inverse">
+  <ul class="nav">
+    <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Welcome <?php echo $r['nama'];?></span><b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="#"><i class="icon-user"></i><?php echo "&nbsp;&nbsp;".$r['nama'];?></a></li>
+        <li><a href="logout.php"><i class="icon-key"></i> Log Out</a></li>
+      </ul>
+    </li>
+    <li class=""><a title="" href="logout.php"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+  </ul>
+</div>
+<!--close-top-Header-menu-->
+<!--start-top-serch-->
+
+<!--close-top-serch-->
+<!--sidebar-menu-->
+<div id="sidebar"><a href="pembayaran.php" class="visible-phone"><i class="icon icon-inbox"></i> <span>pembayaran</span></a>
+  <ul>
+  <?php
+    if($r['id_level'] == 1){
+  ?>
+    <li> <a href="beranda.php"><i class="icon icon-home"></i> <span>Beranda</span></a> </li>
+    <li> <a href="data_mobil.php"><i class="icon icon-tasks"></i> <span>Data Mobil</span></a> </li>
+    <li> <a href="peminjaman.php"><i class="icon icon-shopping-cart"></i> <span>Peminjaman</span></a> </li>
+    <li> <a href="pengembalian.php"><i class="icon icon-inbox"></i> <span>Pengembalian</span></a> </li>
+    <li class="active"> <a href="pembayaran.php"><i class="icon icon-print"></i> <span>Pembayaran</span></a> </li>
+    <li> <a href="logout.php"><i class="icon icon-sign-out"></i> <span>Logout</span></a> </li>
+  <?php
+    } else if($r['id_level'] == 2){
+  ?>
+    <li><a href="beranda.php"><i class="icon icon-home"></i> <span>Beranda</span></a> </li>
+    <li class="active"> <a href="data_mobil_admin.php"><i class="icon icon-tasks"></i> <span>Data Mobil</span></a> </li>
+    <li> <a href="data_transaksi.php"><i class="icon icon-inbox"></i> <span>Data Transaksi</span></a> </li>
+    <li> <a href="logout.php"><i class="icon icon-sign-out"></i> <span>Logout</span></a> </li>
+  <?php
+    }
+  ?>
+  </ul>
+</div>
+<!--sidebar-menu-->
+
+<!--main-container-part-->
+<div id="content">
+<!--breadcrumbs-->
+  <div id="content-header">
+    <div id="breadcrumb"> <a href="pembayaran.php" title="Go to here" class="tip-bottom"><i class="icon icon-print"></i>Pembayaran</a></div>
+  </div>
+<!--End-breadcrumbs-->
+  
+<!--Action boxes-->
+  <div class="container">
+    
+<!--End-Action boxes-->    
+  </div>
+</div>
+
+<!--end-main-container-part-->
+
+<!--Footer-part-->
+
+<div class="row-fluid">
+  <div id="footer" class="span12"> <?php echo date('Y'); ?> &copy; Rent Car <a href="#">Kelompok 9</a> </div>
+</div>
+
+<!--end-Footer-part-->
+
+<script src="template/dashboard/js/excanvas.min.js"></script> 
+<script src="template/dashboard/js/jquery.min.js"></script> 
+<script src="template/dashboard/js/jquery.ui.custom.js"></script> 
+<script src="template/dashboard/js/bootstrap.min.js"></script> 
+<script src="template/dashboard/js/jquery.flot.min.js"></script> 
+<script src="template/dashboard/js/jquery.flot.resize.min.js"></script> 
+<script src="template/dashboard/js/jquery.peity.min.js"></script> 
+<script src="template/dashboard/js/fullcalendar.min.js"></script> 
+<script src="template/dashboard/js/matrix.js"></script> 
+<script src="template/dashboard/js/matrix.dashboard.js"></script> 
+<script src="template/dashboard/js/jquery.gritter.min.js"></script> 
+<script src="template/dashboard/js/matrix.interface.js"></script> 
+<script src="template/dashboard/js/matrix.chat.js"></script> 
+<script src="template/dashboard/js/jquery.validate.js"></script> 
+<script src="template/dashboard/js/matrix.form_validation.js"></script> 
+<script src="template/dashboard/js/jquery.wizard.js"></script> 
+<script src="template/dashboard/js/jquery.uniform.js"></script> 
+<script src="template/dashboard/js/select2.min.js"></script> 
+<script src="template/dashboard/js/matrix.popover.js"></script> 
+<script src="template/dashboard/js/jquery.dataTables.min.js"></script> 
+<script src="template/dashboard/js/matrix.tables.js"></script> 
+
+<script type="text/javascript">
+  // This function is called from the pop-up menus to transfer to
+  // a different page. Ignore if the value returned is a null string:
+  function goPage (newURL) {
+
+      // if url is empty, skip the menu dividers and reset the menu selection to default
+      if (newURL != "") {
+      
+          // if url is "-", it is this page -- reset the menu:
+          if (newURL == "-" ) {
+              resetMenu();            
+          } 
+          // else, send page to designated URL            
+          else {  
+            document.location.href = newURL;
+          }
+      }
+  }
+
+// resets the menu selection upon entry to this page:
+function resetMenu() {
+   document.gomenu.selector.selectedIndex = 2;
+}
+</script>
+</body>
+</html>
+<?php
+  }
+} else {
+  header('location: logout.php');
+}
+ob_flush();
+?>
